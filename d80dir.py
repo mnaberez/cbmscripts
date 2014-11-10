71b7f31eb0ae24a6a8a080b788fb03a4e586a840
Mike Naberezny <mike@naberezny.com>
2014-11-10 12:44:37 -0800

    Replace d80dir with python version

---
+++ d80dir.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+'''Usage: d80dir <filename.d80>'''
+import os
+import subprocess
+import sys
+import tempfile
+
+def print_dir(filename):
+    # if the image has error info at the end, c1541 can't read it,
+    # so create a tempfile without the error info
+    tmpfile = None
+    if filename.endswith('.d80') and os.path.getsize(filename) > 533248:
+        tmpfile = tempfile.NamedTemporaryFile(mode='wb', delete=False)
+        with open(filename, 'rb') as f:
+            data = f.read(533248)
+            tmpfile.write(data)
+            tmpfile.close()
+            filename = tmpfile.name
+
+    # scrape output from c1541
+    proc = subprocess.Popen("c1541 '%s'" % filename, shell=True,
+                            stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    lines = proc.communicate("dir\n")[0].split("\n")
+
+    # remove leading version/copyright info
+    for i, line in enumerate(lines):
+        if line.startswith("c1541 #8>"):
+            break
+    lines = lines[i:]
+
+    # remove prompts and empty lines
+    lines = [ l.replace('c1541 #8> ', '') for l in lines ]
+    lines = [ l for l in lines if len(l.strip()) > 0 ]
+
+    # print the directory
+    for line in lines:
+        print(line)
+
+    # unlink tempfile if we made one
+    if tmpfile:
+        os.unlink(tmpfile.name)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.stderr.write(__doc__ + "\n")
+        sys.exit(1)
+
+    print_dir(sys.argv[1])
